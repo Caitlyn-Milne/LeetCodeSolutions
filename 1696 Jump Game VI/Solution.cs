3c5a9f6b586ee5884581f5740d26240b7d52b93a
Caitlyn-Milne <CaitlynMilne2001@gmail.com>
2022-07-10 23:55:59 +0100

    Added jump game vi Solution

---
+++ 1696 Jump Game VI/Solution.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeSolutions._1696_Jump_Game_VI
+{
+    public class Solution
+    {
+        public int MaxResult1(int[] nums, int k)
+        {
+            LinkedList<int> linklist = new LinkedList<int>();
+            linklist.AddLast(nums[^1]);
+            for (int i = nums.Length - 2; i >= 0; i--)
+            {
+                var max = int.MinValue;
+                foreach (var x in linklist)
+                {                    
+                    if (x > max) max = x;                    
+                }
+                linklist.AddLast(nums[i] + max);
+                if (linklist.Count > k) linklist.RemoveFirst();
+                
+            }
+            return linklist?.Last?.Value ?? 0;
+        }
+
+        public int MaxResult(int[] nums, int k)
+        {
+            PriorityQueue<int,int> heap = new PriorityQueue<int,int>();
+            var max = 0;
+            for (int i = nums.Length - 1; i >= 0; i--)
+            {
+                var prev = 0;
+                while (heap.TryPeek(out var index, out prev)
+                    && index < i - k)
+                {
+                    heap.Dequeue();
+                }
+
+                max = nums[i] + prev;
+                heap.Enqueue(i, max);
+            }
+            return max;
+        }
+    }
+}
